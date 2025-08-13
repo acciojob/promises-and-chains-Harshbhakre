e787e91e249ca5311cd6100c602f218dbee7f36f
Harsh Bhakre <140039147+Harshbhakre@users.noreply.github.com>
2025-08-13 13:09:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+function submitForm() {
+	let nameValue = document.querySelector("#name").value
+	let age = document.querySelector("#age").value
+	if(nameValue=="" || age==""){
+		alert("Please enter valid details.")
+	}else{
+		let result = new Promise((res,rej)=>{
+			if(age>=18){
+				setTimeout(()=>{
+					res("Welcome, . You can vote.")
+				},4000)
+			}else{
+					setTimeout(()=>{
+					rej("Oh sorry . You aren't old enough.")
+				},4000)
+			}
+		})
+		result.then(data=>alert(data)).catch(err=>alert(err))
+	}
+}
