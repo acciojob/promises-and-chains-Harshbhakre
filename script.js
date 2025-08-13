43da335ba377c6feb6b02a2aa9a8c0d099979202
Harsh Bhakre <140039147+Harshbhakre@users.noreply.github.com>
2025-08-13 13:37:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-function submitForm() {
-	let nameValue = document.querySelector("#name").value
-	let age = document.querySelector("#age").value
-	if(nameValue=="" || age==""){
-		alert('Please enter valid details.')
-	}else{
-		let result = new Promise((res, rej) => {
-        if (Number(age) >= 18) {
-            setTimeout(() => {
-                res(`Welcome, ${nameValue}. You can vote.`);
-            }, 4000);
-        } else {
-            setTimeout(() => {
-                rej(`Oh sorry ${nameValue}. You aren't old enough.`);
-            }, 4000);
-        }
-    });
+async function submitForm() {
+    let nameValue = document.querySelector("#name").value.trim();
+    let age = Number(document.querySelector("#age").value);
 
-    result
-        .then(data => alert(data))
-        .catch(err => alert(err));
-}
-}
+    if (!nameValue || !age) {
+        alert("Please enter valid details.");
+        return;
+    }
+
+    await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (age >= 18) {
+                resolve(`Welcome, ${nameValue} You can vote.`);
+            } else {
+                reject(`Oh sorry ${nameValue} You aren't old enough.`);
+            }
+        },4000);
+    })
+    .then(msg => alert(msg))
+    .catch(err => alert(err));
 }
